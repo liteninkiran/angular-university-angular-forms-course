82a20add6cb549c5c9095bb6d29f36c8ecbf2dec
Kiran Anand <liteninkiran@hotmail.com>
2023-09-20 13:05:31 +0100

    023 - Introduction to Reactive Forms - Step-by-Step Example

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormControlOptions, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'login',
@@ -7,11 +8,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginReactiveComponent implements OnInit {
 
+    public form: FormGroup;
+
+    private formOptions: IFormOptionsDef = {} as IFormOptionsDef;
+    private formControls: IFormControlsDef = {} as IFormControlsDef;
+
     constructor() {
 
     }
 
-    ngOnInit() {
-
+    public ngOnInit(): void {
+        this.setupForm();
     }
+
+    private setupForm(): void {
+        this.setFormOptions();
+        this.setFormControls();
+        this.setForm();
+    }
+
+    private setFormOptions(): void {
+        this.formOptions = {
+            email: {
+                initialValue: 'me@domain.com',
+                options: {
+                    validators: [
+                        Validators.required,
+                        Validators.email,
+                    ],
+                }
+            },
+            password: {
+                initialValue: 'pa55word',
+                options: {
+                    validators: [
+                        Validators.required,
+                        Validators.minLength(8),
+                    ],
+                }
+            },
+        }
+    }
+
+    private setFormControls(): void {
+        this.formControls = {
+            email: new FormControl(this.formOptions.email.initialValue, this.formOptions.email.options),
+            password: new FormControl(this.formOptions.password.initialValue, this.formOptions.password.options),
+        }
+    }
+
+    private setForm(): void {
+        this.form = new FormGroup(this.formControls);
+    }
+}
+
+interface IFormOptionsDef {
+    email: {
+        initialValue: string,
+        options: FormControlOptions,
+    },
+    password: {
+        initialValue: string,
+        options: FormControlOptions,
+    },
+}
+
+interface IFormControlsDef {
+    email: FormControl,
+    password: FormControl,
 }
