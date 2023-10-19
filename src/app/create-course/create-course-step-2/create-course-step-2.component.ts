263ce1cf4c7999dc9006126c7766cb2e8e0cc720
Kiran Anand <liteninkiran@hotmail.com>
2023-10-19 22:54:05 +0100

    036 - Reactive Forms - Dynamically Enabling and Disabling Form

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -20,11 +20,22 @@ export class CreateCourseStep2Component implements OnInit {
                 Validators.required,
             ],
         },
+        price: {
+            validators: [
+                Validators.required,
+                Validators.min(1),
+                Validators.max(9999),
+                Validators.pattern('[0-9]+'),
+            ],
+        },
     }
     private formControls = {
         courseType: ['premium', this.formControlDefs.courseType],
+        price: [null as number, this.formControlDefs.price],
     }
     public form: FormGroup<IFormGroupDef> = this.fb.group(this.formControls);
+    public priceControl = this.form.get('price');
+    private price: number;
 
     constructor(
         private fb: FormBuilder,
@@ -33,14 +44,26 @@ export class CreateCourseStep2Component implements OnInit {
     }
 
     public ngOnInit(): void {
-
+        const options = { emitEvent: false }
+        this.form.valueChanges.subscribe((value) => {
+            if (value.courseType === 'free' && this.priceControl.enabled) {
+                this.price = value.price;
+                this.priceControl.setValue(null, options);
+                this.priceControl.disable(options);
+            } else if (value.courseType === 'premium' && this.priceControl.disabled) {
+                this.priceControl.setValue(this.price, options);
+                this.priceControl.enable(options);
+            }
+        });
     }
 }
 
 interface IFormControlDef {
     courseType: AbstractControlOptions;
+    price: AbstractControlOptions;
 }
 
 interface IFormGroupDef {
     courseType: FormControl<string>;
+    price: FormControl<number>;
 }
