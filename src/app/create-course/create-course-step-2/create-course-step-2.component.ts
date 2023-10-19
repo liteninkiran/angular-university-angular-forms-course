90715b52ac2155a4cfa1cdaac034f8282a150fc6
Kiran Anand <liteninkiran@hotmail.com>
2023-10-19 21:31:57 +0100

    035 - Multi-Step Forms - Displaying an Error Message per Step

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControlOptions, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'create-course-step-2',
@@ -8,11 +8,39 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateCourseStep2Component implements OnInit {
 
-    constructor() {
+    public validatorDefs = {
+        title: {
+            minLength: 5,
+            maxLength: 60,
+        },
+    }
+    private formControlDefs: IFormControlDef = {
+        courseType: {
+            validators: [
+                Validators.required,
+            ],
+        },
+    }
+    private formControls = {
+        courseType: ['premium', this.formControlDefs.courseType],
+    }
+    public form: FormGroup<IFormGroupDef> = this.fb.group(this.formControls);
+
+    constructor(
+        private fb: FormBuilder,
+    ) {
 
     }
 
     public ngOnInit(): void {
 
     }
+}
+
+interface IFormControlDef {
+    courseType: AbstractControlOptions;
+}
+
+interface IFormGroupDef {
+    courseType: FormControl<string>;
 }
--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -5,6 +5,14 @@ import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
     selector: 'create-course',
     templateUrl: './create-course.component.html',
     styleUrls: ['./create-course.component.scss'],
+    providers: [
+        {
+            provide: STEPPER_GLOBAL_OPTIONS,
+            useValue: {
+                showError: true,
+            },
+        }
+    ],
 })
 export class CreateCourseComponent implements OnInit {
 
