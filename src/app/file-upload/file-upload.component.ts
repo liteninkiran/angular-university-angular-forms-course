ad566276e5004763334708e6813f0dccfed535ee
Kiran Anand <liteninkiran@hotmail.com>
2023-10-20 13:50:50 +0100

    043 - Angular File Upload - Uploading to a Node Server using the HTTP Client

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -14,10 +14,32 @@ export class FileUploadComponent {
     @Input() public requiredFileType: string;
 
     public fileName: string = '';
+    public fileUploadError = false;
+
+    constructor(
+        private http: HttpClient,
+    ) {
+
+    }
 
     public onFileSelected(event: Event) {
         const target: HTMLInputElement = event.target as HTMLInputElement;
         const file: File = target.files[0];
+        if (!file) {
+            return;
+        }
         this.fileName = file.name;
+        const formData = new FormData();
+        formData.append('thumbnail', file);
+        this.fileUploadError = false;
+        this.http
+            .post('/api/thumbnail-upload', formData)
+            .pipe(
+                catchError((err) => {
+                    this.fileUploadError = true;
+                    return of(err);
+                })
+            )
+            .subscribe();
     }
 }
